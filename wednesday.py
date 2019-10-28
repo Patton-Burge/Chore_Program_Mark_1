d5f80d93c0fd2e8c10b366d831580bd86fda1ec8
Student <student@donation-3.local>
2019-10-28 14:53:12 -0500

    Initialize Chore Program From Personal Computer

---
+++ wednesday.py
@@ -0,0 +1,49 @@
+import json
+from random import choice
+
+
+def load_names():
+    with open("people.json", "r") as file:
+        return json.load(file)
+
+
+def load_chores():
+    with open("chores.json") as chore_file:
+        return json.load(chore_file)
+
+
+def save(day_chores):
+    with open("days/wednesday.json", "w") as daily_chore_file:
+        json.dump(day_chores, daily_chore_file)
+
+
+def main():
+    iterations = 0
+    empty_list = []
+    names = load_names()
+    chores = ["B", "D", "N", "E", "F", "G", "L", "S", "U", "V", "P"]
+    while iterations != 11:
+        name = choice(names)
+        chore = choice(chores)
+        index_of_chore = chores.index(chore)
+        if index_of_chore == 0 or index_of_chore == -1:
+            pass
+        else:
+            index_of_chore + 1
+
+        index_of_name = names.index(name)
+        if index_of_name == -1 or index_of_name == 0:
+            pass
+        else:
+            index_of_name + 1
+        chores.pop(index_of_chore)
+        names.pop(index_of_name)
+        daily_chore = name + " : " + chore
+        if name in daily_chore:
+            iterations += 1
+            empty_list.append(daily_chore)
+    save(empty_list)
+
+
+if __name__ == "__main__":
+    main()
